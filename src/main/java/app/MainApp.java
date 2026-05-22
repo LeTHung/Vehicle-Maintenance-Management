18a2c155aedfa6ca3f8e8d4b7173565f63bd08c5
LeTHung <letienhung2108@gmail.com>
2026-05-23 01:00:45 +0700

    update

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -2,24 +2,42 @@ package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/main-layout.fxml"));
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/view/main-layout.fxml"));
 
-        Scene scene = new Scene(root, 1200, 760);
-        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+            Scene scene = new Scene(root, 1200, 760);
 
-        primaryStage.setTitle("FleetCare - Quản lý hồ sơ & bảo dưỡng phương tiện");
-        primaryStage.setMinWidth(1100);
-        primaryStage.setMinHeight(700);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            String[] globalCssFiles = {
+                    "/css/global/theme.css",
+                    "/css/global/layout.css",
+                    "/css/global/pages.css",
+                    "/css/global/cards.css",
+                    "/css/global/forms.css",
+                    "/css/global/buttons.css",
+                    "/css/global/tables.css"
+            };
+
+            for (String css : globalCssFiles) {
+                scene.getStylesheets().add(
+                        getClass().getResource(css).toExternalForm());
+            }
+
+            stage.setTitle("FleetCare - Quản lý hồ sơ & bảo dưỡng phương tiện");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
