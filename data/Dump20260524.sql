ace2cd806d3b6608e8611a3cf5defec5dde48039
LeTHung <letienhung2108@gmail.com>
2026-05-25 22:40:31 +0700

    doi ten db

--- data/Dump20260524.sql
+++ data/Dump20260524.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `fleet_maintenance_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `fleet_maintenance_db`;
+CREATE DATABASE  IF NOT EXISTS `vehicle_maintenance_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `vehicle_maintenance_management`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: fleet_maintenance_db
